b21061d03032901127af81a92f57f50a9ffd1562
Yudhistir58 <yudhistirr@gmail.com>
2024-04-17 21:04:46 +0200

    Add Player Bet Model

---
+++ Roulette/Roulette/Models/PlayerBetModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Roulette.Models
+{
+    public class PlayerBetModel
+    {
+        public int PlayerBetId { get; set; }
+        public int PlayerId { get; set; }
+        public int BetId { get; set; }
+        public decimal Amount { get; set; }
+        public string Status { get; set; }
+        public  decimal Payout { get; set; }
+        public int ResultId { get; set; }
+
+    }
+}
