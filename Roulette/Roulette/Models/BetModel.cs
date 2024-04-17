d3cb970870ebdcda9af578b90c4dc5d339685f03
Yudhistir58 <yudhistirr@gmail.com>
2024-04-17 20:58:16 +0200

    Add bet model

---
+++ Roulette/Roulette/Models/BetModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Roulette.Models
+{
+    public class BetModel
+    {
+        public int BetId { get; set; }
+        public string BetType { get; set; }
+        public decimal Multiplier { get; set;}
+        public int Number { get; set;}
+    }
+}
